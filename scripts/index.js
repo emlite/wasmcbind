6bf8eedd83592021c658aa1f53ce0de54e056692
Mohammed Alyousef <may642_2000@hotmail.com>
2025-08-05 23:32:45 +0300

    revert

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,11 @@
 import * as webref from "@webref/idl";
 import { clean } from "./clean.js";
 import { generate } from "./gen.js";
-import { postproc } from "./postprocess.js";
 
 async function main() {
   await clean();
   const specAst = await webref.parseAll();
   generate(specAst);
-  await postproc();
   console.log("Generation done!");
 }
 
